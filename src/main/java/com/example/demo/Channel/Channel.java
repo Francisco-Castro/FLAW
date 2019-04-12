e48ee8d408e8517a18e46633a7e3e47a23078bd1
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-04-12 16:59:04 -0400

    Made channel class

--- src/main/java/com/example/demo/Channel/Channel.java
+++ src/main/java/com/example/demo/Channel/Channel.java
@@ -1,6 +1,10 @@
 package com.example.demo.Channel;
 
+import com.example.demo.message.Message;
+import com.example.demo.user.User;
+
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -10,8 +14,45 @@ public class Channel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    //@OneToMany
-    //private List<Message> messages;
-    //@OneToMany
-    //private List<User> users;
+    @OneToMany
+    private List<Message> messages;
+    @OneToMany
+    private List<User> users;
+
+    public Channel(){
+        this.name = "";
+        this.messages = new ArrayList<>();
+        this.users = new ArrayList<>();
+    }
+
+
+    public Channel(String name, List<Message> messages, List<User> users){
+        this.name = name;
+        this.messages = messages;
+        this.users = users;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
 }

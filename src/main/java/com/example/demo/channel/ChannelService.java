8d59d3388ac145b613cbc534183dbbb7a7e2ac1d
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-04-12 19:23:20 -0400

    Finished channel package, channel tests except controller

---
+++ src/main/java/com/example/demo/channel/ChannelService.java
@@ -0,0 +1,40 @@
+package com.example.demo.channel;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChannelService {
+
+    private ChannelRepository channelRepository;
+
+    @Autowired
+    public ChannelService(ChannelRepository channelRepository){
+        this.channelRepository = channelRepository;
+    }
+
+    public Iterable<Channel> index(){
+        return channelRepository.findAll();
+    }
+
+    public Channel show(Long id){
+        return channelRepository.findById(id).get();
+    }
+
+    public Channel create(Channel channel){
+        return channelRepository.save(channel);
+    }
+
+    public Channel update (Long id, Channel channel){
+        Channel original = channelRepository.findById(id).get();
+        original.setName(channel.getName());
+        original.setMessages(channel.getMessages());
+        original.setUsers(channel.getUsers());
+        return channelRepository.save(original);
+    }
+
+    public Boolean delete (Long id){
+        channelRepository.deleteById(id);
+        return true;
+    }
+}
